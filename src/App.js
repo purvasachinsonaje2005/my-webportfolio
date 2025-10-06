0edda9fcd1e8e08e4cda32155ec1e885ff5ab1e3
Purva Sonje <221106003@rcpit.ac.in>
2025-10-06 17:42:32 +0530

    Resolve all merge conflicts and regenerate package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
